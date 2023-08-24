402a79b44abfa99e767c6f5bf96c3542c2a6cca4
odiaivie1 <ivieodia0@gmail.com>
2023-08-24 14:55:32 -0700

    Frees an array of pointers

---
+++ helper.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * free_tok - Frees an array of pointers.
+ */
+void free_tok(void)
+{
+	int i;
+
+	for (i = 0; operation.tokens[i] != NULL; i++)
+		free(operation.tokens[i]);
+	free(operation.tokens);
+}
+
+/**
+ * isnum - checks if string argument represents a number.
+ *
+ * @s: points to string argument.
+ * Return: 1 if s is a number, otherwise 0.
+ */
+int isnum(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (isdigit(s[i]) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
