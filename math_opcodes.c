56e370f5b31e20f127a7ec38fcc5e3273b656f41
teflon40 <frimpong.bernard3220@gmail.com>
2023-08-24 22:24:38 +0000

    Added opcode for multiplication, division and mudular arithmetic

--- math_opcodes.c
+++ math_opcodes.c
@@ -34,3 +34,62 @@ void sub(stack_t **stack, unsigned int number)
 	diff = fpop(stack) - tmp;
 	fpush(stack, diff);
 }
+
+/**
+ * mul - performs multiplication operations
+ *
+ * @stack: a stack.
+ * @number: line number.
+ */
+void mul(stack_t **stack, unsigned int number)
+{
+	int prod;
+
+	if (stack_len(*stack) < 2)
+		err_exit(*stack, "L%d: can't mul, stack too short\n", number);
+
+	prod = fpop(stack) * fpop(stack);
+	fpush(stack, prod);
+}
+
+/**
+ * fdiv - performs division operations
+ *
+ * @stack: a stack.
+ * @number: line number.
+ */
+void fdiv(stack_t **stack, unsigned int number)
+{
+	int top;
+	int quotient;
+
+	if (stack_len(*stack) < 2)
+		err_exit(*stack, "L%d: can't div, stack too short\n", number);
+	if (get_top(*stack) == 0)
+		err_exit(*stack, "L%d: division by zero\n", number);
+
+	top = fpop(stack);
+	quotient = fpop(stack) / top;
+	fpush(stack, quotient);
+}
+
+/**
+ * mod - performs modulus operations
+ *
+ * @stack: a stack.
+ * @number: line number.
+ */
+void mod(stack_t **stack, unsigned int number)
+{
+	int top;
+	int rem;
+
+	if (stack_len(*stack) < 2)
+		err_exit(*stack, "L%d: can't mod, stack too short\n", number);
+	if (get_top(*stack) == 0)
+		err_exit(*stack, "L%d: division by zero\n", number);
+
+	top = fpop(stack);
+	rem = fpop(stack) % top;
+	fpush(stack, rem);
+}
