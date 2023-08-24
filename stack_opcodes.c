b18cf4087e9d257d70382fb62289b4f41a6b07e6
teflon40 <frimpong.bernard3220@gmail.com>
2023-08-24 22:44:52 +0000

    Implement rotr opcode

--- stack_opcodes.c
+++ stack_opcodes.c
@@ -39,3 +39,18 @@ void pop(stack_t **stack, unsigned int number)
 		err_exit(*stack, "L%d: can't pop an empty stack\n", number);
 	fpop(stack);
 }
+
+/**
+ * rotr - rotates the stack to the bottom
+ *
+ * @stack: a stack.
+ * @number: line number.
+ */
+void rotr(stack_t **stack, unsigned int number)
+{
+	UNUSED(number);
+
+	if (stack_len(*stack) > 1)
+		fpush(stack, lpop(stack));
+}
+
