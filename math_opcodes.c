0cb1f22e6f674f9b3f5594ec70efe9f61652a28e
teflon40 <frimpong.bernard3220@gmail.com>
2023-08-24 22:23:10 +0000

    Add opcode for subtraction

--- math_opcodes.c
+++ math_opcodes.c
@@ -17,3 +17,20 @@ void add(stack_t **stack, unsigned int number)
 	fpush(stack, sum);
 }
 
+/**
+ * sub - performs subtraction operations
+ *
+ * @stack: a stack.
+ * @number: line number.
+ */
+void sub(stack_t **stack, unsigned int number)
+{
+	int diff, tmp;
+
+	if (stack_len(*stack) < 2)
+		err_exit(*stack, "L%d: can't sub, stack too short\n", number);
+
+	tmp = fpop(stack);
+	diff = fpop(stack) - tmp;
+	fpush(stack, diff);
+}
